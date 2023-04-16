63ea430ae0c916f074c599afc1065edf7b8a12b4
wuyongchang <wyc@qq.com>
2023-04-16 21:41:54 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,87 @@
+# CDN_IDENTIFY
+
+一款识别域名是否使用cdn的工具
+
+
+
+## 背景
+
+红队打点时经常会有收集子域名然后转成ip进而扩展ip段进行脆弱点寻找的需求，如果域名使用cdn，会导致收集错误的ip段，因此我们需要排除cdn来收集更准确的ip地址。
+
+现有的一些识别cdn的工具存在如下问题：
+
+- *仅根据cname或ip范围判断cdn，cname与ip范围不全导致遗漏*
+
+- *输出字段较多，不方便直接与其他工具结合*
+
+
+
+本工具是 https://github.com/alwaystest18/cdnChecker.git 的python版，原版是go语言的写的，因为个人平时python用的更多，所以将该工具用python写了一份。
+
+
+
+## 安装使用
+
+```
+git https://github.com/assassin-9527/cdn_identify.git
+cd cdn_identify/
+pip install -r requirement.txt
+python3 ./cdn_identify.py --domain www.baidu.com
+```
+
+
+
+## 使用
+
+参数说明
+
+```
+Usage of cdn_identify.py:
+  --domain string         //要测试的域名
+        The target domain
+  --domains string         //要测试的域名列表文件
+        The target domain list file path
+  -o string         //输出文件，默认为out.txt
+        Toutput domains cdn check result to file
+```
+
+单独使用
+
+```
+$ cat domains.txt 
+www.baidu.com        //使用cdn
+www.qq.com         //使用cdn
+www.alibabagroup.com    //使用cdn
+aurora.tencent.com     //未使用cdn
+python3 ./cdn_identify.py --domain www.baidu.com
+python3 ./cdn_identify.py --domains domains.txt
+
+
+**强烈推荐dns服务器列表使用自带的resolvers（均为国内dns服务器且验证可用），如果服务器数量过少，大量的dns查询会导致timeout，影响查询准确度**
+
+
+
+## 识别cdn思路
+
+1.参考 go语言自带库 https://github.com/projectdiscovery/dnsx 的checkCdn方法（通过ip范围判断，主要为国外cdn厂商，对国内cdn识别效果不理想）
+
+2.存在A记录但不存在cname的域名直接判断未使用cdn
+
+3.存在cname的与cdn name列表对比，如果包含cdn cname列表则判断使用cdn
+
+4.主要通过多个dns服务器节点获取域名解析ip，如果存在4个以上不同的ip段，则判断使用cdn，反之未使用cdn。
+
+
+
+## 常见问题
+
+结果中使用cdn域名列表与未使用cdn域名列表数量相加与实际测试域名数量不符？
+
+答：对于无法获取解析ip的域名，程序会默认为域名无效过滤掉
+
+
+
+## 感谢
+
+https://github.com/alwaystest18/cdnChecker.git
+https://github.com/xiaoyiios/chinacdndomianlist
---
+++ cdn_identify.py
@@ -0,0 +1,281 @@
+import requests
+import json
+import netaddr
+import socket
+import argparse
+import datetime
+import threading
+import collections
+import time
+from numpy import random
+from dns import resolver, rdatatype
+
+
+# cdn cname文件路径
+cdn_cname_file_path = "cdn_cname"
+# dns服务器列表文件路径
+resolvers_file_path = "resolvers"
+# 可用dns服务器的最小个数
+min_resolvers_count = 20
+
+def read_file2arr(file_path):
+    """
+    读取文件并以'\n'换行符拆分为数组返回
+    """
+    with open(file=file_path, mode="r", encoding="utf-8") as fd:
+        return fd.readlines()
+
+
+def unique_strlist(str_list:list):
+    """
+    字符列表去重去空
+    """
+    tmp_list = []
+    for str_val in str_list:
+        str = str_val.strip()
+        if str:
+            tmp_list.append(str)
+    return set(tmp_list)
+
+
+class CdnCheck:
+    def __init__(self, resolvers:list, cdn_name_list:list, cache=True) -> None:
+        self.cache = cache
+        self.ranges = {}
+        self.rangers = {}
+        self.resolvers = resolvers
+        self.cdn_name_list = cdn_name_list
+        if cache:
+            self.getCDNDataFromCache()
+        else:
+            self.getCDNData()
+        self.no_cdn_domains = [] # 未使用cdn的域名列表
+        self.use_cdn_domains = [] # 使用cdn的域名列表
+        self.no_cdn_ips = [] # 未使用cdn的ip列表
+    
+    def write_data2file(self, out_file_path, runtime):
+        with open(file=out_file_path, mode="w", encoding="utf-8") as fd:
+            if self.use_cdn_domains:
+                fd.write("# 使用cdn的域名列表\n")
+                fd.writelines('\n'.join(set(self.use_cdn_domains)))
+                fd.write("\n\n")
+            if self.no_cdn_domains:
+                fd.write("# 未使用cdn的域名列表\n")
+                fd.writelines('\n'.join(set(self.no_cdn_domains)))
+                fd.write("\n\n")
+            if self.no_cdn_ips:
+                fd.write("# 未使用cdn的ip列表\n")
+                fd.writelines('\n'.join(set(self.no_cdn_ips)))
+                fd.write("\n\n")
+            fd.write(f'runtime: {runtime}s\n')
+
+
+    def getCDNDataFromCache(self):
+        self.ranges = self.scrapeProjectDiscovery()
+        if self.ranges:
+            for provider, ranges in self.ranges.items():
+                network_list = []
+                for cidr in ranges:
+                    network = netaddr.IPNetwork(cidr)
+                    network_list.append(network)
+                self.rangers[provider] = network_list
+
+    def getCDNData(self):
+        pass
+
+    def scrapeProjectDiscovery(self):
+        data = {}
+        try:
+            resp = requests.get("https://cdn.nuclei.sh")
+            data =json.loads(resp.text)
+        except:
+            pass
+        return data
+    
+    def InCdnCnameList(self, domain_cname_list):
+        for domain_cname in domain_cname_list:
+            for cdn_cname in self.cdn_name_list:
+                if cdn_cname in domain_cname:
+                    return True
+        return False
+    
+    def generateRandomNumber(self,start:int, end:int, count:int):
+        return random.randint(start, end, count)
+    
+    # 获取域名在特定dns上的解析ip，并且以.分割ip，取ip的前三部分，即解析ip为1.1.1.1,最终输出为[1.1.1],便于判断多个ip是否在相同网段
+    def resolvDomainIpPart(self, domain:str, name_server:str):
+        domain_ips_part = []
+        resolverList = [name_server]
+        dns_client = resolver.Resolver()
+        dns_client.nameservers = resolverList
+        try:
+            answers = dns_client.query(qname=domain, rdtype = rdatatype.A)
+            ips_list = []
+            for rdata in answers:
+                ips_list.append(rdata.address)
+            if len(ips_list) > 0:
+                for ip in ips_list:
+                    ip_parts = ip.split(".")
+                    ip_split = ip_parts[0] + "." + ip_parts[1] + "." + ip_parts[2]
+                    domain_ips_part.append(ip_split)
+        except:
+            pass
+        return domain_ips_part
+
+
+    def range_check(self, domain):
+        ip = socket.gethostbyname(domain)
+        for provider, ranger in self.rangers.items():
+            for network in ranger:
+                if ip in network:
+                    return True, provider
+        return False, ""
+
+    def cname_check(self, domain):
+        if self.resolvers:
+            domain_ip_list = []
+            domain_cname_list = []
+            try:
+                dns_client = resolver.Resolver()
+                dns_client.nameservers = self.resolvers
+                answers = dns_client.query(qname=domain, rdtype = rdatatype.A)
+                for answ in answers.response.answer:
+                    for item in answ.items:
+                        if item.rdtype == rdatatype.A:
+                            domain_ip_list.append(item.to_text())
+                        elif item.rdtype == rdatatype.CNAME:
+                            # domain_cname_list.append(item.to_text()[:-1]) 去掉域名最后的点
+                            domain_cname_list.append(item.to_text())
+            except:
+                pass
+            if len(domain_cname_list) == 0 and len(domain_ip_list) > 0:
+                # 无cname但有A记录，直接判定未使用cdn
+                self.no_cdn_domains.append(domain)
+                self.no_cdn_ips.extend(domain_ip_list)
+            elif len(domain_cname_list) > 0 and len(domain_ip_list) > 0:
+                if not self.InCdnCnameList(domain_cname_list):
+                    # cdn在cdn cname列表中包含，直接判定使用cdn
+                    self.use_cdn_domains.append(domain)
+                else:
+                    domain_ip_part_list = []
+                    domain_ip_part_count = 0
+                    rand_nums = self.generateRandomNumber(0, len(self.resolvers), min_resolvers_count)
+                    for num in rand_nums:
+                        name_server = self.resolvers[num]
+                        domain_ips_with_resolver = self.resolvDomainIpPart(domain=domain, name_server=name_server)
+                        domain_ip_part_list.extend(domain_ips_with_resolver)
+                        domain_ip_part_count = len(unique_strlist(domain_ip_part_list))
+                        if domain_ip_part_count > 3:
+                            break
+                    if domain_ip_part_count > 3:
+                        # 不同段ip数量达到4个就判定为使用了cdn
+                        self.use_cdn_domains.append(domain)
+                    else:
+                        self.no_cdn_domains.append(domain)
+                        self.no_cdn_ips.extend(domain_ip_list)
+
+
+        return False
+
+    
+    def Check(self, domain):
+        if domain.strip():
+            is_cdn, _ = self.range_check(domain)
+            if is_cdn:
+                self.use_cdn_domains.append(domain)
+            self.cname_check(domain)
+
+
+
+class WorkThread(object):
+    def __init__(self, targets, concurrency=6):
+        self.concurrency = concurrency
+        self.semaphore = threading.Semaphore(concurrency)
+        self._targets = targets
+        self.result_list = []
+
+    def work(self, target):
+        raise NotImplementedError()
+
+    def _work(self, target):
+        try:
+            ret_val = self.work(target)
+            if ret_val:
+                self.result_list.append(ret_val)
+        except Exception as ex:
+            # print(f"target:{target} [+] ", ex)
+            pass
+
+        except BaseException as ex:
+            print("BaseException on {}".format(str(ex)))
+            self.semaphore.release()
+            raise ex
+        self.semaphore.release()
+
+    def _run(self):
+        deque = collections.deque()
+        for target in self._targets:
+            if isinstance(target, str):
+                target = target.strip()
+            if not target:
+                continue
+            self.semaphore.acquire()
+            t1 = threading.Thread(target=self._work, args=(target,))
+            t1.start()
+            deque.append(t1)
+
+        for t in list(deque):
+            while t.is_alive():
+                time.sleep(0.2)
+    
+    def run(self):
+        self._run()
+        return self.result_list
+
+def filter_valid_resolvers(resolvers_list):
+    # 通过解析特定域名获取ip地址来找出提供列表中可用dns服务器
+    def __filter_valid_worker(name_server):
+        try:
+            dns_client = resolver.Resolver()
+            dns_client.nameservers = [name_server]
+            dns_client.lifetime = 5
+            answers = dns_client.query(qname="public1.114dns.com", rdtype = rdatatype.A)
+            for rdata in answers:
+                if "114.114.114.114" == rdata.address:
+                    return name_server
+        except:
+            pass
+    work_th = WorkThread(resolvers_list, len(resolvers_list))
+    work_th.work = __filter_valid_worker
+    work_th.run()
+    return work_th.result_list
+
+
+
+
+arg_parser = argparse.ArgumentParser() 
+domains_parser = arg_parser.add_mutually_exclusive_group(required=True)
+domains_parser.add_argument('--domain', dest="domain", type=str, help="The target domain")
+domains_parser.add_argument('--domains', dest="domains", type=str, help="The target domain list file path")
+arg_parser.add_argument('-o', dest="out_path", type=str, default="out.txt", help="Toutput domains that are not using cdn to file")
+args = arg_parser.parse_args()
+if __name__ == "__main__":
+    starttime = datetime.datetime.now()
+    cdn_cname_list = unique_strlist(read_file2arr(cdn_cname_file_path))
+    temp_resolvers_list = unique_strlist(read_file2arr(resolvers_file_path))
+    resolvers_list = filter_valid_resolvers(temp_resolvers_list)
+    
+    if len(resolvers_list) < min_resolvers_count:
+        print("The number of valid resolvers is less than 20, please improve it: resolvers")
+        exit(2)
+    check_obj = CdnCheck(resolvers=resolvers_list, cdn_name_list=cdn_cname_list)
+    if args.domain:
+        check_obj.Check(domain=args.domain)
+    elif args.domains:
+        domains_list = unique_strlist(read_file2arr(args.domains))
+        work_th = WorkThread(domains_list, len(domains_list))
+        work_th.work = check_obj.Check
+        work_th.run()
+    endtime = datetime.datetime.now()
+    runtime = (endtime - starttime).seconds
+    check_obj.write_data2file(args.out_path, runtime)
